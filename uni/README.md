4268495b3f9ea9f6cc2d57b8a215b16c9135df25
Paty Arakawa <paty.arakawa@gmail.com>
2019-03-19 11:20:21 -0500

    falta arreglar botón eliminar

--- uni/README.md
+++ uni/README.md
@@ -20,4 +20,4 @@ npm run build --report
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
 
-![Alt text](./static/NuevaCarrera/img.png?raw=true "Title")
+![Alt text](./static/NuevaCarrerag/img.png?raw=true "Title")
--- uni/src/components/HelloWorld.vue
+++ uni/src/components/HelloWorld.vue
@@ -5,19 +5,20 @@
       v-layout.mt-5
         v-flex.xs-12
           label Nombre
-          input.input-career(placeholder="Nombre")
+          input.input-career(placeholder="Nombre", v-model='name')
       v-layout.mt-3
         v-flex.xs-6
           label Código
-          input.input-career
+          input.input-career(v-model='code')
         v-flex.xs-6
           label Facultad
-          select
+          select(v-model='facultie')
             option Seleccione
+            option(v-for='facultie in faculties') {{facultie}}
       h2.mt-3.mb-3.blue--text Cursos por carrera
       v-card.pa-4
         h3 Ciclo 1
-        v-autocomplete(v-model='friends', :disabled='isUpdating', :items='people', chips='', color='', box='', placeholder='Agregar curso a ciclo' item-text='name', item-value='name', multiple='')
+        v-autocomplete(v-model='courses', :disabled='isUpdating', :items='optionCourses', chips='', color='', box='', placeholder='Agregar curso a ciclo' item-text='name', item-value='name', multiple='')
           template(v-slot:selection='data')
             v-chip.chip--select-multi(:selected='data.selected', color='primary', text-color='white' close='', @input='remove(data.item)')
               |{{ data.item.name }}
@@ -27,56 +28,54 @@
             template(v-else='')
               v-list-tile-content
                 v-list-tile-title(v-html='data.item.name')
-        input.input-add.mt-2(placeholder="Agregar curso a ciclo")
-          
+      template
+      .text-xs-center
+        v-dialog(width='500')
+          template(v-slot:activator='{ on }')
+            v-btn(color='red lighten-2', dark='', v-on='on')
+              | Click Me
+          v-card
+            v-card-title.headline.grey.lighten-2(primary-title='')
+              | Datos
+            v-card-text
+              p Nombre: {{name}}
+              p Código: {{code}}
+              p Facultad: {{facultie}}
+              ol 
+                li(v-for='course in courses') {{course}}
+              
 </template>
 
 <script>
 
 export default {
     data () {
-      const srcs = {
-        1: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-        2: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
-        3: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
-        4: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
-        5: 'https://cdn.vuetifyjs.com/images/lists/5.jpg'
-      }
-
       return {
         autoUpdate: true,
-        friends: ['Sandra Adams', 'Britta Holt'],
-        isUpdating: false,
-        name: 'Midnight Crew',
-        people: [
-          { header: 'Group 1' },
-          { name: 'Sandra Adams', group: 'Group 1', avatar: srcs[1] },
-          { name: 'Ali Connors', group: 'Group 1', avatar: srcs[2] },
-          { name: 'Trevor Hansen', group: 'Group 1', avatar: srcs[3] },
-          { name: 'Tucker Smith', group: 'Group 1', avatar: srcs[2] },
-          { divider: true },
-          { header: 'Group 2' },
-          { name: 'Britta Holt', group: 'Group 2', avatar: srcs[4] },
-          { name: 'Jane Smith ', group: 'Group 2', avatar: srcs[5] },
-          { name: 'John Smith', group: 'Group 2', avatar: srcs[1] },
-          { name: 'Sandra Williams', group: 'Group 2', avatar: srcs[3] }
+        courses: [
+          { name: 'Sandra Adams', group: 'Group 1' },
+          { name: 'Ali Connors', group: 'Group 1'},
         ],
-        title: 'The summer breeze'
-      }
-    },
+        isUpdating: false,
+        name: '',
+        code: '',
+        facultie: '',
+        optionCourses: [
+          { header: 'Group 1' },
+          { name: 'Sandra Adams', group: 'Group 1' },
+          { name: 'Ali Connors', group: 'Group 1'},
+          { name: 'Trevor Hansen', group: 'Group 1' },
+          { name: 'Tucker Smith', group: 'Group 1'},
+        ],
+        faculties: ['Matemática', 'Educación'],
 
-    watch: {
-      isUpdating (val) {
-        if (val) {
-          setTimeout(() => (this.isUpdating = false), 3000)
-        }
       }
     },
 
     methods: {
       remove (item) {
-        const index = this.friends.indexOf(item.name)
-        if (index >= 0) this.friends.splice(index, 1)
+        const index = this.courses.name.indexOf(item.name)
+        if (index >= 0) this.courses.splice(index, 1)
       }
     }
   }
@@ -132,7 +131,6 @@ export default {
   .v-input input 
     max-height: 27px;
     width: 3em;
-    background: red;
 
   .v-autocomplete__content.v-menu__content, .v-autocomplete__content.v-menu__content .v-card
     min-width: auto !important;
